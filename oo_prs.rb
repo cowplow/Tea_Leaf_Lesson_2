af026b31f625b7a32b5f016224043be65414a64e
Chris Trembley <cmt2057@rit.edu>
2014-11-22 14:38:05 -0500

    Refactored rps code after watching solution

--- oo_prs.rb
+++ oo_prs.rb
@@ -1,66 +1,83 @@
 #oo_prs.rb
 
-class Player
-  attr_accessor :choice, :name
+class PlayerHand
+  include Comparable
+  attr_reader :name, :choice
 
-  def initialize(name)
-    @choice = ""
-    @name = name
+  def <=>(another_player)
+    if self.choice == another_player.choice
+      0
+    elsif (self.choice == 'r' && another_player.choice == 's') || (self.choice == 's' && another_player.choice == 'p') ||
+      (self.choice == 'p' && another_player.choice == 'r')
+      1
+    else
+      -1
+    end
+  end
+
+  def initialize(n)
+    @name = n
+  end
+
+  def to_s
+    puts "#{name} has selected #{Game::CHOICES[choice]}"
+  end
+
+end
+
+class Human < PlayerHand
+
+  def pick_hand
+    puts "Hi #{name}, please pick: Rock, Paper, or Scissors. (P/R/S)"
+    player_choice = gets.chomp.downcase
+
+    until Game::CHOICES.keys.include?(player_choice)
+      puts "Invalid Choice.  Please pick: 'P', 'R', or 'S'"
+      player_choice = gets.chomp.downcase
+    end
+    @choice = player_choice
+
+  end
+
+end
+
+class Computer < PlayerHand 
+
+  def pick_hand
+    comp_choice = Game::CHOICES.keys.sample
+    @choice = comp_choice
+  end
+
+end
+
+class Game
+  attr_reader :computer, :player
+
+  CHOICES = { 'r' => "Rock", 's' => "Scissors", 'p' => "Paper"}
+
+  def initialize
+    @player = Human.new("Chris")
+    @computer = Computer.new("C3PO")
+  end
+
+  def compare_hands
+    if player == computer
+      puts "It's a tie"
+    elsif player > computer
+      puts "#{player.name} wins"
+    else
+      puts "#{computer.name} wins"
+    end
+  end
+        
+
+  def play
+    player.pick_hand
+    computer.pick_hand
+    player.to_s
+    computer.to_s
+    compare_hands
   end
 end
 
-def get_user_choice(player)
-  puts "Hi #{player.name} choose one: Paper, Rock, or Scissors. (P/R/S)"
-  user_choice = gets.chomp.upcase
-  until user_choice == 'R' || user_choice == 'P' || user_choice == 'S'
-    puts "Invalid input. Acceptable inputs are: P, R, or S"
-    puts "Please choose again"
-    user_choice = gets.chomp.upcase
-  end
-  player.choice = user_choice
-end
-
-def computer_choice
-  choices = ["R", "P", "S"]
-  comp_choice = choices.sample
-end
-
-def determine_outcome(player, comp_choice)
-  if player.choice == comp_choice
-    puts "It's a tie!"
-  elsif player.choice == 'R' && comp_choice == 'P'
-    puts "Sorry #{player.name}, Paper wraps Rock.  You lose this round!"
-  elsif player.choice == 'P' && comp_choice == 'S'
-    puts "Sorry #{player.name}, Scissors cuts Paper.  You lose this round!"
-  elsif player.choice == 'S' && comp_choice == 'R'
-    puts "Sorry #{player.name}, Rock smashes Scissors.  You lose this round!"
-  elsif player.choice == 'R'
-    puts "Congratulations #{player.name}, Rock smashes Scissors.  You win this round"
-  elsif player.choice == 'P'
-    puts "Congratulations #{player.name}, Paper wraps Rock. You win this round!"
-  else
-    puts "Congratulations #{player}, Scissors cuts Paper. You win this round!"
-  end
-end
-      
-      
-puts "Welcome to OO Rock Paper Scissors.  Can we have your name please?"
-
-input = gets.chomp      
-      
-player = Player.new(input)
-
-begin
-  get_user_choice(player)
-
-  comp_choice = computer_choice
-
-  determine_outcome(player, comp_choice)
-
-  puts "Would you like to play again? (Y/N)"
-
-  again = gets.chomp.upcase
-
-end while again == 'Y'
-
-
+Game.new.play
